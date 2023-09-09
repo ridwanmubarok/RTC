6ae38fcf542068d260cb17f175a8fe54ee1a23f1
ridwanmubarok <ridwanmubarok5@gmail.com>
2023-09-10 04:15:50 +0700

    initial

---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const SimplePeer = require('simple-peer');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+const port = process.env.PORT || 3000;
+
+
+// Objek untuk menyimpan informasi tentang setiap ruang (room)
+const rooms = {};
+
+// Atur socket.io
+io.on('connection', (socket) => {
+  console.log('Klien terhubung');
+
+  socket.on('disconnect', () => {
+    console.log('Klien terputus');
+    
+    // Keluar dari ruang saat klien terputus
+    const room = socket.room;
+    if (room && rooms[room]) {
+      rooms[room].delete(socket.id);
+    }
+  });
+
+  // Event untuk bergabung ke ruang (room) baru
+  socket.on('join-room', (roomName) => {
+    socket.join(roomName);
+    socket.room = roomName;
+
+    // Membuat objek set (untuk menghindari duplikat) untuk setiap ruang
+    if (!rooms[roomName]) {
+      rooms[roomName] = new Set();
+    }
+    rooms[roomName].add(socket.id);
+  });
+
+  // Handle event untuk memulai panggilan video
+  socket.on('start-call', (targetSocketId) => {
+    const room = socket.room;
+
+    if (!room || !rooms[room]) {
+      socket.emit('room-not-found');
+      return;
+    }
+
+    const targetSocket = Array.from(rooms[room]).find(
+      (id) => id === targetSocketId
+    );
+
+    if (!targetSocket) {
+      socket.emit('user-not-found');
+      return;
+    }
+
+    // Buat instance simple-peer untuk inisiasi panggilan
+    const initiatorPeer = new SimplePeer({ initiator: true });
+    const targetPeer = new SimplePeer();
+
+    // Kirim offer dari initiator ke target
+    initiatorPeer.on('signal', (offer) => {
+      io.to(targetSocket).emit('offer', offer);
+    });
+
+    // Kirim answer dari target ke initiator
+    targetPeer.on('signal', (answer) => {
+      socket.emit('answer', answer);
+    });
+
+    // Terima stream video dari initiator dan kirimkan ke target
+    initiatorPeer.on('stream', (stream) => {
+      io.to(targetSocket).emit('stream', stream);
+    });
+
+    // Terima stream video dari target dan kirimkan ke initiator
+    targetPeer.on('stream', (stream) => {
+      socket.emit('stream', stream);
+    });
+
+    // Terima sinyal offer dari target
+    socket.on('offer', (offer) => {
+      targetPeer.signal(offer);
+    });
+
+    // Terima sinyal answer dari initiator
+    socket.on('answer', (answer) => {
+      initiatorPeer.signal(answer);
+    });
+  });
+});
+
+// Mulai server Express
+server.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
