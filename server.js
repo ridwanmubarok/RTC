24a40696b8547f15b535ea37f00e03d817037c02
ridwanmubarok <ridwanmubarok5@gmail.com>
2023-09-10 17:13:49 +0700

    update

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-const SimplePeer = require('simple-peer');
-const wrtc = require('wrtc'); // Import modul wrtc
 
 const app = express();
 const server = http.createServer(app);
@@ -21,121 +19,29 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
-  res.header('Access-Control-Allow-Methods', 'GET,POST');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
 // Objek untuk menyimpan informasi tentang setiap ruang (room)
 const rooms = {};
 
-// Penanganan WebRTC Peer
-function startCallInRoom(roomName, socket) {
-  const peer = new SimplePeer({ initiator: true, wrtc: wrtc }); // Penginisiasi panggilan dengan wrtc
-
-  peer.on('signal', (data) => {
-    // Kirim sinyal ke klien lain
-    socket.to(roomName).emit('signal', data);
-  });
-
-  peer.on('stream', (stream) => {
-    // Kirim stream video ke klien lain
-    socket.to(roomName).emit('stream', stream);
-  });
-
-  // Koneksi ke sinyal masuk dari klien lain
-  socket.on('signal', (data) => {
-    peer.signal(data);
-  });
-
-  // Menerima panggilan dari klien lain
-  socket.on('call', () => {
-    if (!socket.isInCall) {
-      socket.isInCall = true;
-      navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-        .then((stream) => {
-          peer.addStream(stream); // Tambahkan stream ke peer
-          socket.emit('stream', stream); // Kirim stream video Anda sendiri
-        })
-        .catch((error) => {
-          console.error('Error accessing media devices:', error);
-        });
-    }
-  });
-}
-
-// Atur socket.io
+// Event handler ketika klien terhubung
 io.on('connection', (socket) => {
-  console.log('Klien terhubung');
-
-  // Tambahkan properti untuk melacak apakah pengguna sudah memulai panggilan
-  socket.isInCall = false;
-
-  socket.on('disconnect', () => {
-    console.log('Klien terputus');
-    // Keluar dari ruang saat klien terputus
-    const room = socket.room;
-    if (room && rooms[room]) {
-      rooms[room].delete(socket.id);
-    }
-  });
-
-  // Event untuk bergabung ke ruang (room) atau membuat baru jika belum ada
   socket.on('join-room', (roomName) => {
-    if (!rooms[roomName]) {
-      // Jika ruang belum ada, maka buat ruang baru
-      rooms[roomName] = new Set();
-      socket.join(roomName);
-      socket.room = roomName;
-      rooms[roomName].add(socket.id);
-      socket.emit('room-joined', roomName);
-    } else if (rooms[roomName].size < 2) {
-      // Jika ruang kurang dari 2 pengguna, pengguna dapat bergabung
-      socket.join(roomName);
-      socket.room = roomName;
-      rooms[roomName].add(socket.id);
-      socket.emit('room-joined', roomName);
-    } else {
-      // Jika ruang sudah penuh, coba cari atau buat ruang baru
-      let newRoomName = roomName;
-      let roomNumber = 2;
-      while (rooms[newRoomName] && rooms[newRoomName].size >= 2) {
-        newRoomName = roomName + roomNumber;
-        roomNumber++;
-      }
-    
-      // Jika ditemukan atau dibuat ruang baru, pengguna dapat bergabung
-      if (!rooms[newRoomName]) {
-        rooms[newRoomName] = new Set();
-      }
-      socket.join(newRoomName);
-      socket.room = newRoomName;
-      rooms[newRoomName].add(socket.id);
-      socket.emit('room-joined', newRoomName);
-    }
-    
-    // Jika sudah ada 2 pengguna di ruang ini, dan pengguna ini belum memulai panggilan, maka mulailah panggilan
-    if (rooms[roomName] && rooms[roomName].size === 2 && !socket.isInCall) {
-      socket.isInCall = true;
-      startCallInRoom(roomName, socket);
-    }
+    socket.join(roomName);
+    io.to(roomName).emit('user-connected', socket.id);
+    socket.on('disconnect', () => {
+      io.to(roomName).emit('user-disconnected', socket.id);
+    });
+    socket.on('message', (message) => {
+      io.to(roomName).emit('message', message);
+    });
   });
 });
 
 // Mulai server Express
 server.listen(port, () => {
   console.log(`Server berjalan di http://localhost:${port}`);
-});
-
-// Routing untuk mendapatkan daftar ruang yang ada
-app.get('/api/rooms', (req, res) => {
-  const roomList = [];
-  for (const roomName in rooms) {
-    const participantCount = rooms[roomName].size;
-    roomList.push({ name: roomName, participant: participantCount });
-  }
-  res.json(roomList);
 });
