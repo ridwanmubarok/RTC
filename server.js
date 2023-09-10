56f8e8279ed65ff21e385ae3f059157e4de4461f
ridwanmubarok <ridwanmubarok5@gmail.com>
2023-09-10 18:36:26 +0700

    dialog

--- server.js
+++ server.js
@@ -1,47 +1,39 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-
+const express = require("express");
 const app = express();
-const server = http.createServer(app);
-const io = socketIO(server, {
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+app.set("view engine", "ejs");
+const io = require("socket.io")(server, {
   cors: {
-    origin: ['https://amubhya.test', 'https://katakreasi.com'],
-    methods: ["GET", "POST"]
+    origin: '*'
   }
 });
-const port = process.env.PORT || 3000;
+const { ExpressPeerServer } = require("peer");
+const options = { // Perbaiki nama variabel ke "options"
+  debug: true,
+}
 
-// Middleware untuk mengatasi CORS - Hapus middleware ini jika Anda telah mengkonfigurasi CORS di socket.io
-app.use((req, res, next) => {
-  const allowedOrigins = ['https://amubhya.test', 'https://katakreasi.com'];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  res.header('Access-Control-Allow-Methods', 'GET, POST');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+app.use("/peerjs", ExpressPeerServer(server, options));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
 });
 
-// Objek untuk menyimpan informasi tentang setiap ruang (room)
-const rooms = {};
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
 
-// Event handler ketika klien terhubung
-io.on('connection', (socket) => {
-  socket.on('join-room', (roomName) => {
-    socket.join(roomName);
-    io.to(roomName).emit('user-connected', socket.id);
-    socket.on('disconnect', () => {
-      io.to(roomName).emit('user-disconnected', socket.id);
-    });
-    socket.on('message', (message) => {
-      io.to(roomName).emit('message', message);
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId, userName) => {
+    socket.join(roomId);
+    setTimeout(() => {
+      io.to(roomId).emit("user-connected", userId);
+    }, 1000)
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message, userName); // Menggunakan userName dalam emit pesan
     });
   });
 });
 
-// Mulai server Express
-server.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
-});
+server.listen(process.env.PORT || 3030);
